8bff8263c61d3043bfb85447341b981a68bafe9f
Mohamed Islam Arrar <mohamedarrar953@gmail.com>
2023-05-14 14:11:44 +0000

    add splash widget + app colors class

---
+++ lib/Core/Style Utils/AppColors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static Color purpleColor = Color(0xff2C2078);
+  static Color lightGreen = Color(0xff43C29C);
+  static Color lightRed = Color(0xffE87565);
+  static Color redColor = Color(0xffFD372A);
+  static Color beigeColor = Color(0xffF9BB98);
+}
