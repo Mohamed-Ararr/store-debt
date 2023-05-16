21645d0eab817542fe3db58708764654eaa9d484
Mohamed Islam Arrar <mohamedarrar953@gmail.com>
2023-05-16 12:06:17 +0000

    add AllDebtsView widgets

---
+++ lib/Features/AllDebt Screen/Presentation/AllDebtsView.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+import 'Widgets/AllDebtsViewBody.dart';
+
+class AllDebtsView extends StatelessWidget {
+  const AllDebtsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const AllDebtsViewBody();
+  }
+}
---
+++ lib/Features/AllDebt Screen/Presentation/Widgets/AllDebtsViewBody.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AllDebtsViewBody extends StatelessWidget {
+  const AllDebtsViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
