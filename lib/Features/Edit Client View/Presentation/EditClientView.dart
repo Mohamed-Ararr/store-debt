9013d5d7f737cb20438fbc88095c62d63e618666
Mohamed Islam Arrar <mohamedarrar953@gmail.com>
2023-05-18 15:53:26 +0000

    add EditClientView widgets

---
+++ lib/Features/Edit Client View/Presentation/EditClientView.dart
@@ -0,0 +1,12 @@
+import "package:flutter/material.dart";
+
+import "Widgets/EditClientViewBody.dart";
+
+class EditClientView extends StatelessWidget {
+  const EditClientView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const EditClientViewBody();
+  }
+}
---
+++ lib/Features/Edit Client View/Presentation/Widgets/EditClientViewBody.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class EditClientViewBody extends StatelessWidget {
+  const EditClientViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
