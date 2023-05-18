5bb318f1d61011f7b170690c0e0a7298abb9de2c
Mohamed Islam Arrar <mohamedarrar953@gmail.com>
2023-05-18 16:16:52 +0000

    add slidable effect to tall client card

--- lib/Features/Home Screen/Presentation/Widgets/TallClientCard.dart
+++ lib/Features/Home Screen/Presentation/Widgets/TallClientCard.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+import 'package:go_router/go_router.dart';
 
 import '../../../../Constants.dart';
+import '../../../../Core/Routing Utils/routes.dart';
 import '../../../../Core/Style Utils/AppColors.dart';
 import 'TallClientInfo.dart';
 
@@ -9,18 +12,61 @@ class TallClientCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: () {},
-      child: Container(
-        padding: EdgeInsets.symmetric(horizontal: 5, vertical: 5),
-        margin: EdgeInsets.symmetric(horizontal: 5),
-        width: MediaQuery.of(context).size.width * 0.28,
-        decoration: BoxDecoration(
-          border: Border.all(color: AppColors.whiteColor),
-          borderRadius: bRadius10,
+    return Row(
+      children: [
+        Slidable(
+          direction: Axis.vertical,
+          startActionPane: ActionPane(
+            extentRatio: 1,
+            motion: const BehindMotion(),
+            children: [
+              SlidableAction(
+                onPressed: (context) =>
+                    GoRouter.of(context).push(AppRoutes.editClientView),
+                borderRadius: bRadius10.copyWith(
+                  bottomLeft: Radius.zero,
+                  bottomRight: Radius.zero,
+                ),
+                backgroundColor: AppColors.greyColor,
+                foregroundColor: Colors.white,
+                icon: Icons.edit,
+                label: 'تعديل',
+              ),
+              SlidableAction(
+                onPressed: (context) {},
+                backgroundColor: AppColors.lightGreen,
+                foregroundColor: Colors.white,
+                icon: Icons.paid_outlined,
+                label: 'تم الدفع',
+              ),
+              SlidableAction(
+                onPressed: (context) {},
+                borderRadius: bRadius10.copyWith(
+                  topRight: Radius.zero,
+                  topLeft: Radius.zero,
+                ),
+                backgroundColor: AppColors.redColor,
+                foregroundColor: Colors.white,
+                icon: Icons.delete_outline,
+                label: 'حذف',
+              ),
+            ],
+          ),
+          child: InkWell(
+            onTap: () {},
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 5, vertical: 5),
+              width: MediaQuery.of(context).size.width * 0.28,
+              decoration: BoxDecoration(
+                border: Border.all(color: AppColors.whiteColor),
+                borderRadius: bRadius10,
+              ),
+              child: TallClientInfo(),
+            ),
+          ),
         ),
-        child: TallClientInfo(),
-      ),
+        SizedBox(width: 10),
+      ],
     );
   }
 }
--- lib/Features/Home Screen/Presentation/Widgets/TallClientCardListView.dart
+++ lib/Features/Home Screen/Presentation/Widgets/TallClientCardListView.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+import 'package:go_router/go_router.dart';
 
+import '../../../../Constants.dart';
+import '../../../../Core/Routing Utils/routes.dart';
+import '../../../../Core/Style Utils/AppColors.dart';
 import 'TallClientCard.dart';
 
 class TallClientCardListView extends StatelessWidget {
