c57d79c6b5a22ed48c47e69740f0f5e4036c28df
Mohamed Islam Arrar <mohamedarrar953@gmail.com>
2023-05-18 14:35:10 +0000

    add AddNewDebtViewBody widgets

---
+++ lib/Features/Add New Debt View/Presentation/Widgets/AddNewDebtViewBody.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AddNewDebtViewBody extends StatelessWidget {
+  const AddNewDebtViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
