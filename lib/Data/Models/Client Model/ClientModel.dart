d2d7b53f26c77556e2bf3889a4c6ce5bd65bec08
Mohamed Islam Arrar <mohamedarrar953@gmail.com>
2023-05-20 14:44:47 +0000

    add type adapter for client model

--- lib/Data/Models/Client Model/ClientModel.dart
+++ lib/Data/Models/Client Model/ClientModel.dart
@@ -1,8 +1,18 @@
-class ClientModel {
+import 'package:hive_flutter/hive_flutter.dart';
+
+part 'ClientModel.g.dart';
+
+@HiveType(typeId: 1)
+class ClientModel extends HiveObject {
+  @HiveField(0)
   String clientFN;
+  @HiveField(1)
   String clientLN;
+  @HiveField(2)
   String price;
+  @HiveField(3)
   bool isPaid;
+  @HiveField(4)
   String date;
 
   ClientModel({
