969f2b9fd15c8819b757f864c644c8416b059273
Mohamed Islam Arrar <mohamedarrar953@gmail.com>
2023-05-20 14:33:17 +0000

    add owner model

--- lib/Data/Models/ClientModel.dart
+++ lib/Data/Models/ClientModel.dart
@@ -1,4 +1,3 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
 class ClientModel {
   String clientFN;
   String clientLN;
---
+++ lib/Data/Models/OwnerModel.dart
@@ -0,0 +1,9 @@
+class OwnerModel {
+  String ownerFN;
+  String ownerLN;
+
+  OwnerModel({
+    required this.ownerFN,
+    required this.ownerLN,
+  });
+}
