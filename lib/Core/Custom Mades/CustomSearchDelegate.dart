6da96350afc80fadb61f1138ec5dbd50117b2bf0
Mohamed Islam Arrar <mohamedarrar953@gmail.com>
2023-05-22 16:03:34 +0000

    search feature done

--- lib/Core/Custom Mades/CustomSearchDelegate.dart
+++ lib/Core/Custom Mades/CustomSearchDelegate.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:go_router/go_router.dart';
+import 'package:storedebt/Constants.dart';
+import 'package:storedebt/Core/General%20Utils/EmptyListWidget.dart';
+import 'package:storedebt/Core/General%20Utils/LoadingWidget.dart';
 import 'package:storedebt/Core/Style%20Utils/AppColors.dart';
 import 'package:storedebt/Core/Style%20Utils/AppFonts.dart';
+import 'package:storedebt/Data/Cubits/Client%20Cubit/Fetch%20Client%20Cubit/client_cubit.dart';
+import 'package:storedebt/Data/Models/Client%20Model/ClientModel.dart';
+import 'package:storedebt/Features/AllDebt%20Screen/Presentation/Widgets/DebtClientCard.dart';
+
+import '../../Features/AllDebt Screen/Presentation/Widgets/DebtClientCardWithSlidable.dart';
+import '../../Features/AllDebt Screen/Presentation/Widgets/DebtListView.dart';
 
 class CustomSearch extends SearchDelegate {
   @override
@@ -47,11 +57,37 @@ class CustomSearch extends SearchDelegate {
 
   @override
   Widget buildResults(BuildContext context) {
-    return Text("Result");
+    return buildSuggestions(context);
   }
 
   @override
   Widget buildSuggestions(BuildContext context) {
-    return Text("Suggestation");
+    return BlocBuilder<ClientCubit, ClientState>(builder: (context, state) {
+      if (state is ClientSuccess) {
+        List<ClientModel> suggestions = state.clientList.where((client) {
+          final result = "${client.clientFN} ${client.clientLN}".toLowerCase();
+          final input = query.toLowerCase();
+          return result.contains(input);
+        }).toList();
+        if (state.clientList.isEmpty) {
+          return EmptyListWidget(style: AppFonts.regular20BoldBlack);
+        } else {
+          return Padding(
+            padding: paddingAll15,
+            child: ListView.builder(
+              itemCount: suggestions.length,
+              physics: BouncingScrollPhysics(),
+              itemBuilder: (context, index) {
+                return DebtClientCardWithSlidable(client: suggestions[index]);
+              },
+            ),
+          );
+        }
+      } else if (state is ClientFailure) {
+        return Center(child: Text(state.errorMsg));
+      } else {
+        return LoadingWidget();
+      }
+    });
   }
 }
